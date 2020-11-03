902ed84c194cbb5e767b754d95a04ea1caa31823
Tyrese Merrick <merricktyrese@gmail.com>
2020-11-03 13:28:56 -0600

    Auto stash before merge of "Tyrese2" and "origin/master"

--- src/main/TicketPrinter.java
+++ src/main/TicketPrinter.java
@@ -1,6 +1,24 @@
 package main;
 
+import java.util.Scanner;
+
 public class TicketPrinter {
 
+<<<<<<< Updated upstream
 
+=======
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name");
+        Scanner
+        System.out.println("Enter your last name");
+        System.out.println("Enter your email");
+        System.out.println("Enter your phone_number");
+        System.out.println("Enter your age");
+        System.out.println("Enter your destination");
+        System.out.println("Enter your departure time");
+
+    }
+>>>>>>> Stashed changes
 }
